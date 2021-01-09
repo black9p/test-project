5c15a974cd9eb157c1b96e2fe680d440d2519b76
black9p <black9p@naver.com>
2021-01-09 18:45:04 +0900

    Add update post

--- src/main/kotlin/me/black9p/testproject/web/IndexController.kt
+++ src/main/kotlin/me/black9p/testproject/web/IndexController.kt
@@ -4,6 +4,7 @@ import me.black9p.testproject.service.PostsService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 /**
  * @author black9p
@@ -20,5 +21,11 @@ class IndexController (val postsService: PostsService){
     @GetMapping("/posts/save")
     fun postsSave(): String {
         return "posts-save"
+    }
+
+    @GetMapping("/posts/update/{id}")
+    fun postsUpdate(@PathVariable id:Long, model: Model): String {
+        model.addAttribute("post", postsService.getPost(id))
+        return "posts-update"
     }
 }
