e4bf18c10238b3c3777c9ec9a4154e3c5621b007
black9p <black9p@naver.com>
2021-01-14 00:00:12 +0900

    Add Login

--- src/main/kotlin/me/black9p/testproject/domain/user/User.kt
+++ src/main/kotlin/me/black9p/testproject/domain/user/User.kt
@@ -1,18 +1,21 @@
 package me.black9p.testproject.domain.user
 
 import me.black9p.testproject.domain.BaseTimeEntity
-import javax.persistence.EnumType
-import javax.persistence.Enumerated
+import javax.persistence.*
 
 /**
  * @author black9p
  */
+@Entity
 class User(
     var name: String,
     var email: String,
     var picture: String,
     @Enumerated(EnumType.STRING) var role: Role
 ) : BaseTimeEntity() {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
 
     fun update(name: String, picture: String): User {
         this.name = name
--- src/main/kotlin/me/black9p/testproject/web/IndexController.kt
+++ src/main/kotlin/me/black9p/testproject/web/IndexController.kt
@@ -1,20 +1,25 @@
 package me.black9p.testproject.web
 
+import me.black9p.testproject.config.auth.dto.SessionUser
 import me.black9p.testproject.service.PostsService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import javax.servlet.http.HttpSession
 
 /**
  * @author black9p
  */
 @Controller
-class IndexController (val postsService: PostsService){
+class IndexController (val postsService: PostsService, val httpSession: HttpSession){
 
     @GetMapping("/")
     fun index(model: Model): String {
         model.addAttribute("posts", postsService.getPostsFromRecent())
+
+        val user = httpSession.getAttribute("user") as SessionUser
+        model.addAttribute("userName", user.name)
         return "index"
     }
 
